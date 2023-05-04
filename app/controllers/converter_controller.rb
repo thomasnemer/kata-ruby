7dc643efa35fa08e4bea54f275aeaa6b78fb6fc7
Thomas Nemer <thomas.nemer@doctolib.com>
2023-05-04 17:44:17 +0200

    feat: params validation

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,4 +1,29 @@
 class ConverterController < ApplicationController
+  before_action :validate, only: [:convert]
   def index
+  end
+
+  def convert
+  end
+
+  def validate
+    if validate_quantity
+      puts toto
+    end
+    validate_currency(params['source_currency'])
+    validate_currency(params['target_currency'])
+  end
+
+  def validate_quantity?
+    begin
+      Float(params['quantity'])
+    rescue
+      return false
+    end
+    return true
+  end
+
+  def validate_currency(currency)
+    currency in %w[EUR BTC DOGE USD YEN]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@
 
 Rails.application.routes.draw do
   root 'converter#index'
+  post '/', to: 'converter#convert'
 end
