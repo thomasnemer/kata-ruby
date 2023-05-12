28046d417c15ab81f145f228e8eba21bde47a0a6
zelha <zineb.elhaouari@doctolib.com>
2023-05-12 15:58:18 +0200

    validated, call conversion api

--- app/controllers/converter_controller.rb
+++ app/controllers/converter_controller.rb
@@ -1,29 +1,36 @@
+require 'net/http'
+require 'json'
+
 class ConverterController < ApplicationController
   before_action :validate, only: [:convert]
   def index
   end
 
   def convert
+    # https://exchangerate.host/#/docs
+    url = "https://api.exchangerate.host/convert?from=#{params['source_currency']}&to=#{params['target_currency']}&amount=#{params['quantity']}"
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+    response_obj = JSON.parse(response)
+
+    # debugger
+    render :index
   end
 
   def validate
-    if validate_quantity
-      puts toto
-    end
+    @errors = ActiveModel::Errors.new(self)
+    validate_quantity
     validate_currency(params['source_currency'])
     validate_currency(params['target_currency'])
   end
 
-  def validate_quantity?
-    begin
-      Float(params['quantity'])
-    rescue
-      return false
-    end
-    return true
+  def validate_quantity
+    Float(params['quantity'])
+  rescue
+    @errors.add :quantity, :not_a_number, message: "Quantity must be a number"
   end
 
   def validate_currency(currency)
-    currency in %w[EUR BTC DOGE USD YEN]
+    @errors.add :currency, :unknown_currency, message: "Currency #{currency} is not in EUR BTC DOGE USD YEN " unless currency in %w[EUR BTC DOGE USD YEN]
   end
 end
